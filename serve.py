6011bde7637d2f45cf7e4cd2b35eba7216a079e6
majidaldo <majidaldosari-github@yahoo.com>
2015-02-11 01:51:51 -0500

    termination of subprocs

--- serve.py
+++ serve.py
@@ -1,35 +1,53 @@
 from flask import Flask ,redirect, url_for
 import subprocess
+import os
 app = Flask(__name__)
 
 #todo somehow hide the :808x from the user so that
 #todo func to remove and kill child procs that closd
 running_displays={}
-import socket
-def rundisplay(display=0):
+
+from time import sleep
+def rundisplay(display=8888):
     """runs the html5 part of the app returning the port number"""
-    port = 8080+display
+    try: kill_display(display)
+    except: pass
+    port = display
     #one  display at a time
     if display in running_displays:
         return port()
     else:
         running_displays[display]=subprocess.Popen(
-		['./display.sh',str(display)])
+        ['./display.sh',str(display)]
+        ,preexec_fn=os.setsid
+        )
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     #block until 'app' is ready on the port
-    while (0 != s.connect_ex(('127.0.0.1', port))): continue
-    
+    while ( isport_running(port) is not True ): sleep(.1); continue
+
     return port
 
-def isdisplay_running(display):
+import socket
+def isport_running(port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    cr=s.connect_ex(('127.0.0.1', port))
+    if cr==0: return True
+    else: return cr
+
+import signal
+@app.route('/kill')
+def kill_display(display=8888):
+    p= running_displays[display]
+    os.killpg(p.pid, signal.SIGTERM)#p.send_signal(signal.SIGINT);
+    p.wait()
+    running_displays.pop(display)
+    return 'kiling'
 
 @app.route("/boweb")
-def boweb():
-    print rundisplay() #since only one display at a time...
+def boweb(): #since only one display at a time...
     #.. no problem from multiple hits from the same user
-    print 'asdfdsfsf'
-    return redirect('http://localhost:8080',code=307)
+    rundisplay()
+    return redirect('http://localhost:8888',code=307)
 
 	
 @app.route("/")
