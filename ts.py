7706a2c0493090e41522c0de047bc6598a49b858
majidaldo <majidaldosari-github@yahoo.com>
2015-02-12 15:41:12 -0500

    tornado websocket experiment init

---
+++ ts.py
@@ -0,0 +1,12 @@
+import tornado
+from tornado import websocket
+
+class EchoWebSocket(tornado.websocket.WebSocketHandler):
+    def open(self):
+        print("WebSocket opened")
+
+    def on_message(self, message):
+        self.write_message(u"You said: " + message)
+
+    def on_close(self):
+        print("WebSocket closed")
