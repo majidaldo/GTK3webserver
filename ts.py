0fc17a664585314a1bbaa6366ba59190ac8bfae7
majidaldo <majidaldosari-github@yahoo.com>
2015-02-14 15:37:31 -0500

    aah ok NOW timeout works

--- ts.py
+++ ts.py
@@ -5,30 +5,77 @@ import tornado.web
 
 import time
 from time import sleep
+
 class alive(tornado.websocket.WebSocketHandler):
-    def check_origin(self,origin):return True
+    def check_origin(self,origin):return True #for ver 4
 
     def open(self):
-        self.pt=tornado.ioloop.PeriodicCallback(self.poll,7000);
+        self.dueclose=True
+        self.pt=tornado.ioloop.PeriodicCallback(self.poll
+		     ,1000*( 0*3600 +0*60  +7  ));
         self.pt.start() # PollTimer
         print('opened ws')
         #self.write_message(u"confirmcontinue")
         pass
 
+		
     def on_message(self, message):
-        print(message)
-        if message=="pleasedontclose": self.pt.start()
-        else:  self.close()
-        #self.write_message(u"You said2: " + message)
+        print self.dueclose, message
+        if message=='pleasedontclose':
+            self.dueclose=False; self.pt.start()
+        else: self.dueclose==True ; self.closeifdue()
+        # if self.dueclose==True:
+            # if message=='pleasedontclose':
+                # self.pt.start(); self.dueclose=True; return
+            # #elif: message=='close': self.pt.stop(); self.close(); return
+            # else: self.pt.stop(); self.close(); return
+        # else:#have to be explicit bc it was hard!
+            # if message=='pleasedontclose':
+                # self.pt.start(); self.dueclose=True; return
+            # else: self.pt.stop(); self.close(); return
+        #if already closed 
+        #if self.closeswitch==True: return
+            # if message=='pleasedontclose': return
+            # else: 
+        # if message=='close' and self.closeswitch==True:return
+        # else: self.closeswitch=True
+        # #now just respond to the switch
+        # if self.closeswitch==False: self.pt.start();
+        # else: self.pt.stop(); self.close()
+        
+        #onlyu responding to pleasedontclose
+        #since i responded , now switch it back
+        #if message=='pleasedontclose': self.closeswitch=True
+        #else: self.closeswitch=False
 
+            # if message!='pleasedontclose'
+        # if self.lastmessage=='close':
+            # message=
+        # #now just respond to the switch
+        # if self.lastmessage=='close': return #already closed
+        # if ((self.lastmessage=='pleasedontclose' \
+           # or self.lastmessage=='') \
+            # and message=='close'): self.close()
+        # if message=="pleasedontclose": self.pt.start()
+        # else:  self.close()
+        # #self.write_message(u"You said2: " + message)
+
+    def closeifdue(self):
+        if self.dueclose==True: self.close()
+		
     def poll(self):
+        self.dueclose=True
+        #self.closeswitch=False #since im asking, dont close
         self.pt.stop()
         self.write_message(u"confirmcontinue")
         #response timer
-        self.rt=iolp.call_later(3,lambda : self.on_message('close'))
-        
+        self.rt=iolp.call_later(3
+		       #,lambda : self.on_message('close')
+                ,self.closeifdue
+				)
 
-    def on_close(self):
+    def on_close(self): #but not wehen this obj closes
+        self.dueclose=True
         print("WebSocket closed2")
         #self.pt.stop() ; del self.pt #do i need to del?
 
