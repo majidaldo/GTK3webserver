dc36a816c0d22003d6402383ae9a00f452d5a869
majidaldo <majidaldosari-github@yahoo.com>
2015-02-15 15:39:40 -0500

    need to put in displayport

--- serve.py
+++ serve.py
@@ -2,16 +2,18 @@ import tornado
 from tornado import websocket
 import tornado.ioloop
 import tornado.web
-
+import atexit
 import time
-from time import sleep
+#from time import sleep
+from random import getrandbits
 
 polltime= 0*3600 +0*60  +10  # seconds chks usr active
 responsetime= 5 #seconds response time given to usr
 
 class alive(tornado.websocket.WebSocketHandler):
+    clients={} #id:thisinst
     def check_origin(self,origin):return True #for ver 4
-
+	
     def open(self):
         self.dueclose=True
         self.pt=tornado.ioloop.PeriodicCallback(self.poll
@@ -19,9 +21,15 @@ class alive(tornado.websocket.WebSocketHandler):
         self.pt.start() # PollTimer
         pass
 
-		
     def on_message(self, message):
-        if message=='pleasedontclose':
+        print(message)
+        if 'REQ_ID='==message[:7]:
+            self.id=message[7:]
+            self.clients[self.id]=self
+            #todo chk the id is the same one that was given
+            #if self.id in application.handlers.dispalyhandler
+            #    .self.clients[self.id]=self
+        elif message=='pleasedontclose':
             #should rename dueclose to signify that it
 			#blocks the response timer callback. but since
 			#it works as is, i dont care! it was a pain
@@ -43,38 +51,60 @@ class alive(tornado.websocket.WebSocketHandler):
 
     def on_close(self): #but not wehen this obj closes
         self.dueclose=True
+        self.clients.pop(self.id) #pop other 'clientdicts too
         print("WebSocket closed2")
 
-#tornado.httpserver.socket.gethostname()
-#def put_broadwayaddr
-#need that last slash like localhost:8080/
-#open('broadway.html').read().replace('$BROADWAY_SERVER',WITH)
+apps={'boweb':  {'cmd':'python bo.py'},
+      'gedit': {'cmd':'gedit'}
+	  }
+display_spec={'gedit':['gedit'], 'bo':['boweb']
+          ,'boNgedit':['bo','gedit']
+		  }
 
+
+#https for the websockets?
+import uuid
 import display
-class MainHandler(tornado.web.RequestHandler):
+class DisplayHandler(tornado.web.RequestHandler):
+    apps=[]
+    clients={} # id:hdlrinst
+    def prepare(self):
+        self.id=uuid.uuid4()
+        self.clients[self.id]=self
+        self.display_num,self.display_port=display.add() #display.get_openport
+        #problem: if browser didnt get back?
+	    #tst by corrupting html
+        display.app('gedit',self.display_num)
+        hn=tornado.httpserver.socket.gethostname()
+        self.html=open('broadway.html').read()\
+	        .replace('$BROADWAY_SERVER'
+	            ,"'http://"+hn+':'+str(self.display_port)+"/'")\
+            .replace('$REQ_ID',str(self.id))
+	    #need that last slash in url localhost:8080/        
     def get(self):
-       dn,prt=display.add()
-       #problem: if browser didnt get back?
-	   #tst by corrupting html
-       svr=tornado.httpserver.socket.gethostname()
-       html=self.write(open('broadway.html').read().\
-	   replace('$BROADWAY_SERVER'
-	   ,"'http://"+svr+':'+str(lp)+"/'"))
-       self.write(unicode(html))
+        self.write((self.html)) #no need for unicode?
     #make func: is my assoc ws alive??
-	   
+    def on_finish(self):
+        self.clients.pop(self.id)
+
+#def make_DisplayHandler(display_spec):
+#    class dh(DisplayHandler
+
 application = tornado.web.Application([
-    (r"/", MainHandler)
+    (r"/", DisplayHandler)
     ,(r'/wsalive',alive)
 ]
 ,debug=True
 )
 
-from random import randint
+def printstuff():pass
+
+atexit.register( display.kill_all )
 
 if __name__ == "__main__":
     lp=8888
     application.listen(lp)
     iolp=tornado.ioloop.IOLoop.instance()
+    tornado.ioloop.PeriodicCallback(printstuff,10*1000)
     iolp.start()
 	
